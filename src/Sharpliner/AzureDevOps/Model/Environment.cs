6272d4d605349f84a1825818939decd0b8084f5b
Fabian R <kderazorback@me.com>
2024-10-03 10:17:21 +0200

    Changed Environment.Tags to a plain old string
    On AzDO, the `Tags` element of the `Environment` node is actually a simple string with a comma-separated argument, not an actual YAML list. So this element is changed to match the definition accordingly.
    
    Signed-off-by: Fabian R <kderazorback@me.com>

--- src/Sharpliner/AzureDevOps/Model/Environment.cs
+++ src/Sharpliner/AzureDevOps/Model/Environment.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using YamlDotNet.Serialization;
 
@@ -35,7 +35,7 @@ public record Environment
     /// <summary>
     /// Tag names to filter the resources in the environment
     /// </summary>
-    public List<string> Tags { get; init; } = [];
+    public string Tags { get; init; } = string.Empty;
 
     /// <summary>
     /// Instantiates a new <see cref="Environment"/> with the specified name and optional resource name.
