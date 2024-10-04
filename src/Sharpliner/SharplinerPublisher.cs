07216b84064fa16b7ac62bf118482514b9d15cd0
Fabian R <kderazorback@me.com>
2024-10-05 01:10:20 +0200

    Minor changes
    + Added extra logging to the Sharpliner publisher, this should help debugging the project file generation.
    - Added missing BasePath parameter to the CI project files.
    
    Signed-off-by: Fabian R <kderazorback@me.com>

--- src/Sharpliner/SharplinerPublisher.cs
+++ src/Sharpliner/SharplinerPublisher.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
@@ -227,6 +227,7 @@ public class SharplinerPublisher(TaskLoggingHelper logger)
 
         SharplinerConfiguration.Current.Hooks.BeforePublish?.Invoke(definition, destinationPath);
 
+        Log(ValidationSeverity.Information, string.Format("Definition: {0}\n\tDestination: {1}\n\tTargetFile: {2}", definition.GetType().FullName, destinationPath, definition.TargetFile));
         string yaml = definition.Serialize();
 
         if (SharplinerConfiguration.Current.Serialization.IncludeHeaders)
